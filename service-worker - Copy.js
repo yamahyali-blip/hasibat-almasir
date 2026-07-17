3d5b520378e580e0f897da1c70421bca1761c656
yamahyali-blip <yamahyali@gmail.com>
2026-07-17 08:32:41 +0400

    ترتيب القائمة والحقول في صفوف

---
+++ service-worker - Copy.js
@@ -0,0 +1,28 @@
+const CACHE = "movement-calculator-v5";
+const CORE = [
+  "./","./index.html","./styles.css?v=5","./app.js?v=5","./manifest.webmanifest",
+  "./assets/mbz-camo.jpg","./assets/zayed.jpg","./assets/forces-logo.png",
+  "./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+  const req = event.request;
+  const isPage = req.mode === "navigate" || req.destination === "document";
+  if(isPage){
+    event.respondWith(fetch(req).then(res => {
+      const copy=res.clone();caches.open(CACHE).then(c => c.put("./index.html",copy));return res;
+    }).catch(() => caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(fetch(req).then(res => {
+    const copy=res.clone();caches.open(CACHE).then(c => c.put(req,copy));return res;
+  }).catch(() => caches.match(req)));
+});
